6279a2268f0a0a0249c28e6e17888448ea2a9f0e
vikram <vikram.nov.14@gmail.com>
2017-04-05 18:52:38 +0530

    transfer learning with vgg16

---
+++ Vgg16.py
@@ -0,0 +1,112 @@
+import tensorflow as tf
+import numpy as np
+
+VGG_MEAN = [103.939, 116.779, 123.68]
+
+class Vgg16:
+    
+    data_dict = None
+    '''
+    Load the numpy model file into a dictionary
+    '''
+    def __init__(self, rgbImagesPlaceholder, model_path=None):
+        self.data_dict = np.load(model_path, encoding='latin1').item()
+        self._buildVgg16FeatureExtractor(rgbImagesPlaceholder)
+        print("The model has been loaded...")
+
+    '''
+    The VGG16 network uses mena subtracted images in BGR format as it used OpenCV which loaded image as BGR.
+    '''
+    def _preprocessImageAsPerVGG16(self,rgbImage):
+        red, green, blue = tf.split(axis=3, num_or_size_splits=3, value=rgbImage)
+        bgrImage = tf.concat(axis=3, values=[
+            blue - VGG_MEAN[0],
+            green - VGG_MEAN[1],
+            red - VGG_MEAN[2],
+        ])
+        return bgrImage
+
+    def getFinalLayer(self):
+        return self.pool5
+    '''
+    Build the model using the loaded weights
+    '''
+    def _buildVgg16FeatureExtractor(self, rgbImages):
+        
+        bgrMeanSubtractedImage = self._preprocessImageAsPerVGG16(rgbImages)
+
+        self.conv1_1 = self._conv_layer(bgrMeanSubtractedImage, "conv1_1")
+        self.conv1_2 = self._conv_layer(self.conv1_1, "conv1_2")
+        self.pool1 = self._max_pool(self.conv1_2, 'pool1')
+
+        self.conv2_1 = self._conv_layer(self.pool1, "conv2_1")
+        self.conv2_2 = self._conv_layer(self.conv2_1, "conv2_2")
+        self.pool2 = self._max_pool(self.conv2_2, 'pool2')
+
+        self.conv3_1 = self._conv_layer(self.pool2, "conv3_1")
+        self.conv3_2 = self._conv_layer(self.conv3_1, "conv3_2")
+        self.conv3_3 = self._conv_layer(self.conv3_2, "conv3_3")
+        self.pool3 = self._max_pool(self.conv3_3, 'pool3')
+
+        self.conv4_1 = self._conv_layer(self.pool3, "conv4_1")
+        self.conv4_2 = self._conv_layer(self.conv4_1, "conv4_2")
+        self.conv4_3 = self._conv_layer(self.conv4_2, "conv4_3")
+        self.pool4 = self._max_pool(self.conv4_3, 'pool4')
+
+        self.conv5_1 = self._conv_layer(self.pool4, "conv5_1")
+        self.conv5_2 = self._conv_layer(self.conv5_1, "conv5_2")
+        self.conv5_3 = self._conv_layer(self.conv5_2, "conv5_3")
+        self.pool5 = self._max_pool(self.conv5_3, 'pool5')
+        
+        # self.fc6 = self._fc_layer(self.pool5, "fc6")
+        # self.relu6 = tf.nn.relu(self.fc6)
+
+        # self.fc7 = self._fc_layer(self.relu6, "fc7")
+        # self.relu7 = tf.nn.relu(self.fc7)
+
+        # self.fc8 = self._fc_layer(self.relu7, "fc8")
+
+        # self.prob = tf.nn.softmax(self.fc8, name="prob")
+
+        # self.data_dict = None
+
+        return self.pool5
+
+        print "VGG16 Feature Extractor Model Built...."
+
+
+    def _conv_layer(self, input, name):
+        with tf.variable_scope(name):
+            filt = self._get_conv_filter(name)
+            conv = tf.nn.conv2d(input, filt, [1, 1, 1, 1], padding='SAME')
+            conv_biases = self._get_bias(name)
+            bias = tf.nn.bias_add(conv, conv_biases)
+            relu = tf.nn.relu(bias)
+            return relu
+
+    def _fc_layer(self, input, name):
+        with tf.variable_scope(name):
+            shape = input.get_shape().as_list()
+            dim = 1
+            for d in shape[1:]:
+                dim *= d
+            x = tf.reshape(input, [-1, dim])
+            biases = self._get_bias(name)
+            weights = self._get_fc_weight(name)
+            fc = tf.nn.bias_add(tf.matmul(x, weights), biases)
+            return fc
+
+    def _get_fc_weight(self, name):
+        return tf.constant(self.data_dict[name][0], name="weights")
+
+    def _get_conv_filter(self, name):
+    	return tf.constant(self.data_dict[name][0], name="filter")
+
+    def _get_bias(self, name):
+    	return tf.constant(self.data_dict[name][1], name="biases")
+
+    def _max_pool(self, bottom, name):
+    	return tf.nn.max_pool(bottom, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME', name=name)
+
+    def predict(self,rgbImages,session):
+        return session.run([self.prob], feed_dict={self.rgbImages: rgbImages})
--- convNetModel.py
+++ convNetModel.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from config import *
+from Vgg16 import *
 
 class convNetModel:
 
@@ -12,11 +13,11 @@ class convNetModel:
     y = None
     keep_rate = None
 
-    def __init__(self):
+    def __init__(self,usePretrainedNetwork=False):
         self.x = tf.placeholder('float', [None, imageSizeX,imageSizeY,numChannels])
         self.y = tf.placeholder('float')
         self.keep_rate = tf.placeholder(tf.float32)
-        self._setupNetwork()
+        self._setupNetwork(usePretrainedNetwork)
 
     def _conv2d(self,x, W):
         return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
@@ -26,19 +27,16 @@ class convNetModel:
 
 
     def _buildNetwork(self):
-        weights = {'W_conv1': tf.Variable(tf.random_normal([5, 5, numChannels, 32])),
+	weights = {'W_conv1': tf.Variable(tf.random_normal([5, 5, numChannels, 32])),
                    'W_conv2': tf.Variable(tf.random_normal([5, 5, 32, 64])),
-                   'W_conv3': tf.Variable(tf.random_normal([5, 5, 64, 64])),
-                   'W_fc': tf.Variable(tf.random_normal([imageSizeX/8 * imageSizeY/8 * 64, 1024])),
+                   'W_fc': tf.Variable(tf.random_normal([imageSizeX/4 * imageSizeY/4 * 64, 1024])),
                    'out': tf.Variable(tf.random_normal([1024, n_classes]))}
 
-        biases = {'b_conv1': tf.Variable(tf.random_normal([32])),
+	biases = {'b_conv1': tf.Variable(tf.random_normal([32])),
                   'b_conv2': tf.Variable(tf.random_normal([64])),
-                  'b_conv3': tf.Variable(tf.random_normal([64])),
                   'b_fc': tf.Variable(tf.random_normal([1024])),
                   'out': tf.Variable(tf.random_normal([n_classes]))}
 
-        #x = tf.reshape(x, shape=[-1, 28, 28, 1])
 
         conv1 = tf.nn.relu(self._conv2d(self.x, weights['W_conv1']) + biases['b_conv1'])
         conv1 = self._maxpool2d(conv1)
@@ -57,8 +55,39 @@ class convNetModel:
 
         return output
 
-    def _setupNetwork(self):
-        self.prediction = self._buildNetwork()
+    def _buildNetworkFromPretrainedVGG16(self):
+            
+            weights = {'out': tf.Variable(tf.random_normal([1024, n_classes]))}
+            biases = {'out': tf.Variable(tf.random_normal([n_classes]))}
+
+            vgg16FeatureExtrator = Vgg16(self.x, pretrainedModelPath)
+            pretrainedNetwork = vgg16FeatureExtrator.getFinalLayer()
+            
+            # Here, we are converting the output from the pretrained network [num_images,reduced_xdim,reduced_ydim,final_num_filters]
+            # into [num_images,reduced_xdim * reduced_ydim * final_num_filters]
+            shape = pretrainedNetwork.get_shape().as_list()
+            dim = 1
+            for d in shape[1:]:
+                dim *= d
+            fc  = tf.reshape(pretrainedNetwork, [-1, dim])
+            
+            # First fully connected layer
+            w_fc_1 = tf.Variable(tf.random_normal([dim, 1024]))
+            b_fc_1 = tf.Variable(tf.random_normal([1024]))
+            fc = tf.nn.relu(tf.matmul(fc, w_fc_1) + b_fc_1)
+            fc = tf.nn.dropout(fc, self.keep_rate)
+            
+            # Output layer
+            output = tf.matmul(fc, weights['out']) + biases['out']
+            return output
+
+    def _setupNetwork(self,usePretrainedNetwork):
+        
+        if usePretrainedNetwork:
+            self.prediction = self._buildNetworkFromPretrainedVGG16()
+        else:
+            self.prediction = self._buildNetwork()
+
         self.cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.prediction, labels=self.y))
         self.optimizer = tf.train.AdamOptimizer().minimize(self.cost)
         self.correct = tf.equal(tf.argmax(self.prediction, 1), tf.argmax(self.y, 1))
